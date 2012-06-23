267802edfb7b843cb4f3875ed273887733fc993c
manu <manu@manu.(none)>
2012-06-23 12:26:24 -0300

    prueba de conexion

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,6 +1,11 @@
 package com.example;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletConfig;
@@ -11,15 +16,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+import t4p.persistence.ConnectionProvider;
+
+
 public class HelloServlet extends HttpServlet {
 
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        
+         PrintWriter out = resp.getWriter();
 
-        out.write("Hello Heroku by priscilaaa && manu".getBytes());
+/**/
+        try{
+	        Connection connectionManager = ConnectionProvider.getConnection();
+	    	
+			PreparedStatement statement = connectionManager
+					.prepareStatement("select '1' as prueba");
+	
+			ResultSet result = statement.executeQuery();
+			
+			if (result.next()) {
+				
+				out.write(result.getInt("prueba"));
+				
+			}
+        }catch(Exception ex){
+        	
+        }
+/**/
         
         out.flush();
         out.close();
--- src/main/java/t4p/persistence/ConnectionProvider.java
+++ src/main/java/t4p/persistence/ConnectionProvider.java
@@ -9,7 +9,10 @@ public class ConnectionProvider {
 	static {
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			theConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/PersonProject?" + "user=root&password=mysql" );
+			String connParam = "jdbc:postgresql://localhost/test?user=fred&password=secret";
+			//theConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/PersonProject?" + "user=root&password=mysql" );
+			//theConnection = DriverManager.getConnection("jdbc:postgresql://localhost:3306/aaarrgh?user=postgres");
+			theConnection = DriverManager.getConnection("jdbc:postgresql://ec2-23-23-223-68.compute-1.amazonaws.com:3306/zjvhatsnun?user=zjvhatsnun&password=sMxj1CQfWzFEFpMhx1xJ");
 		}catch (Exception e){
 			
 		}
